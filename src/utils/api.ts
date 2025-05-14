0c2c147cfea43d6f2a128e144c9374fe610d52ff
GaziWani1 <gaziwani123@gmail.com.com>
2025-05-14 14:32:30 +0530

    server url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // const API_URL = import.meta.env.VITE_LOCAL_BACKEND_URL;
 const API_URL = import.meta.env.VITE_SERVER_BACKEND_URL;
+console.log(API_URL);
 
 const api = axios.create({
   baseURL: API_URL,
